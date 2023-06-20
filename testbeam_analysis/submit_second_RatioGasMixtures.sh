6a6ed4a47b8aa8c1593273d9bfd4b38e6e1dae49
Brunella D'Anzi <75045014+bdanzi@users.noreply.github.com>
2023-06-20 18:59:13 +0200

    Add files via upload

---
+++ testbeam_analysis/submit_second_RatioGasMixtures.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /lustrehome/bdanzi/cmsset_default.sh
+cd CMSSW_10_6_22/src/
+eval `scramv1 runtime -sh`
+cd ../..
+source setDCDataReaderEnv.sh
+root -l -b ReadSglChannel_test_NovTestBeam_RatioGasMixtures_ClusterCorrected_lastversion.C -q
